7266b30a90d6f4185987f6a812c5a5bf907adb91
Adriel Renato <adriel.renato26@gmail.com>
2025-03-16 15:24:35 -0300

    Criando entidades, dto, controller repositorio de pacientes para fazer o cadastro de pacientes!

---
+++ src/main/java/med/voll/api/model/Paciente.java
@@ -0,0 +1,38 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.dto.DadosCadastroPaciente;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity()
+@Table(name = "pacientes")
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+    }
+
+}
