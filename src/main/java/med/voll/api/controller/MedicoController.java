b93587454103d7bd999ae8cb903cc1b22125107f
Adriel Renato <adriel.renato26@gmail.com>
2025-03-16 14:43:46 -0300

    Implementação do repositorio de médico que extende a jpa e já cria os metodos para utilizar o banco de dados e criação dos contrutores para fazer a conversão do dto para atributos da entidades medico e endereço!

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.dto.DadosCadastroMedico;
+import med.voll.api.model.Medico;
+import med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico json) {
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 }
--- src/main/java/med/voll/api/model/Endereco.java
+++ src/main/java/med/voll/api/model/Endereco.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.dto.DadosEndereco;
 
 @Embeddable
 @Getter
@@ -17,4 +18,14 @@ public class Endereco {
     private String uf;
     private String numero;
     private String complemento;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
 }
