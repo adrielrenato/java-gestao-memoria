216def72da25e2d21e25a322c930faab58b8cc0f
Adriel Renato <adriel.renato26@gmail.com>
2025-03-18 21:20:47 -0300

    Criando cenario de teste onde da sucesso 200 e retorna o agendamento da consulta, criação de uma classe de configuração de mock bean para não utilizar o banco de dados!

--- src/test/java/med/voll/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -1,27 +1,51 @@
 package med.voll.api.controller;
 
+import med.voll.api.domain.consulta.dto.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.dto.DadosDetalhamentoConsulta;
+import med.voll.api.domain.consulta.service.AgendamentoDeConsulta;
+import med.voll.api.domain.medico.enums.Especialidade;
+import med.voll.api.infra.config.TestBeansConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+@Import(TestBeansConfig.class)
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJson;
+
+    @Autowired
+    private AgendamentoDeConsulta agendamentoDeConsulta;
+
     @Test
-    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+    @DisplayName("Deveria devolver codigo http 400 quando informacoes estão invalidas")
     @WithMockUser
     void agendarCenario1() throws Exception {
         var response = mvc.perform(post("/consultas"))
@@ -29,5 +53,34 @@ class ConsultaControllerTest {
                 .getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deveria devolver codigo http 200 quando informacoes estão validos")
+    @WithMockUser
+    void agendarCenario2() throws Exception {
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, "8ab375d6-2477-46e9-808c-ce97b20a3d3b", "8a186641-bce5-41be-9bf9-64400dd97bcb", data);
+
+        when(agendamentoDeConsulta.agendar(any())).thenReturn(dadosDetalhamento);
+
+        var response = mvc.perform(
+                    post("/consultas")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(dadosAgendamentoConsultaJson.write(
+                                    new DadosAgendamentoConsulta("8ab375d6-2477-46e9-808c-ce97b20a3d3b", "8a186641-bce5-41be-9bf9-64400dd97bcb", data, especialidade)
+                            ).getJson())
+                )
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoConsultaJson.write(
+                dadosDetalhamento
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
